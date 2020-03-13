2dad689e46918150177d58779b04ab50ebf9da9a
sebizart <sebizart@gmail.com>
2020-03-13 09:29:07 +0100

    b

---
+++ Assets/Scripts/Network/NM_NetworkManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NM_NetworkManager : MonoBehaviourPunCallbacks
+{
+    //
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        RoomOptions _roomSettings = new RoomOptions()
+        {
+            MaxPlayers = 7
+        };
+        PhotonNetwork.JoinOrCreateRoom("O3D", _roomSettings, new TypedLobby("O3D", LobbyType.Default));
+    }
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("Joined room O3D");
+        PhotonView _id = PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity).GetPhotonView();
+        _id.name = _id.ViewID.ToString();
+    }
+    //
+    void Connect()
+    {
+        PhotonNetwork.PhotonServerSettings.AppSettings.AppVersion = "2.3";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    //
+    private void OnGUI()
+    {
+        GUILayout.Box(PhotonNetwork.NetworkClientState.ToString());
+        GUILayout.Box(PhotonNetwork.IsMasterClient.ToString());
+        GUILayout.Box(PhotonNetwork.CurrentRoom?.Name);
+        GUILayout.Box($"{PhotonNetwork.CurrentRoom?.PlayerCount}/{PhotonNetwork.CurrentRoom?.MaxPlayers}");
+        if (GUILayout.Button("Join"))
+            Connect();
+    }
+}
---
+++ Assets/Scripts/Network/NM_PlayerNetwork.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NM_PlayerNetwork : MonoBehaviour, IPunObservable
+{
+    [SerializeField] PhotonView myID = null;
+    Vector3 localMovement = Vector3.zero;
+
+    void Start()
+    {
+        name = myID.ViewID.ToString();
+    }
+
+    void Update()
+    {
+        if(myID.IsMine)
+            OnLocalMovement();
+        else 
+            OnOnlineMovement();
+    }
+
+    void OnLocalMovement()
+    {
+        localMovement = transform.position;
+    }
+    void OnOnlineMovement()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, localMovement, Time.deltaTime * 20);
+    }
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.IsWriting)
+        {
+            stream.SendNext(localMovement.x);
+            stream.SendNext(localMovement.y);
+            stream.SendNext(localMovement.z);
+        }
+        else
+        {
+            localMovement.x = (float)stream.ReceiveNext();
+            localMovement.y = (float)stream.ReceiveNext();
+            localMovement.z = (float)stream.ReceiveNext();
+        }
+    }
+
+
+}
