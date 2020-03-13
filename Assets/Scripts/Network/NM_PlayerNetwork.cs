c199b1c3ad657795555616c35c3407568ad4fbcf
William Morelli <morelli186@gmail.com>
2020-03-13 11:28:12 +0100

    finish rotation to debug

--- Assets/Scripts/Network/NM_PlayerNetwork.cs
+++ Assets/Scripts/Network/NM_PlayerNetwork.cs
@@ -7,47 +7,44 @@ using Photon.Realtime;
 public class NM_PlayerNetwork : MonoBehaviour, IPunObservable
 {
     [SerializeField] PhotonView myID = null;
-    Vector3 localMovement = Vector3.zero;
+
+    NM_RotationMovment rotation = null;
 
     void Start()
     {
+        rotation = new NM_RotationMovment(gameObject);
+
         name = myID.ViewID.ToString();
+        
     }
 
     void Update()
     {
         if(myID.IsMine)
-            OnLocalMovement();
+        {
+            rotation.OnLocalRotation();
+
+        }
         else 
-            OnOnlineMovement();
+        {
+            rotation.OnLineRotation();
+
+        }
     }
 
-    void OnLocalMovement()
-    {
-        localMovement = transform.position;
-    }
-    void OnOnlineMovement()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, localMovement, Time.deltaTime * 20);
-    }
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if(stream.IsWriting)
         {
-            SendElement(stream, localMovement.x);
-            SendElement(stream, localMovement.y);
-            SendElement(stream, localMovement.z);
+
+            rotation.Send(stream);
+            
         }
+
         else
         {
-            ReceiveElement(stream, ref localMovement.x);
-            ReceiveElement(stream, ref localMovement.y);
-            ReceiveElement(stream, ref localMovement.z);
+
         }
     }
-
-    void SendElement<T>(PhotonStream stream, T _element) => stream.SendNext(_element);
-    void ReceiveElement<T>(PhotonStream stream, ref T _element) => _element = (T)stream.ReceiveNext();
-
 
 }
